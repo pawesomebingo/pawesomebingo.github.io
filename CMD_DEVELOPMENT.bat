69883dbdd9e14c1f18eb7d280affa669d6930785
tnguyen-win <129979622+tnguyenwin@users.noreply.github.com>
2026-03-10 12:24:35 -0700

    Wrangler Setup

--- CMD_DEVELOPMENT.bat
+++ CMD_DEVELOPMENT.bat
@@ -1,14 +1,19 @@
 @ECHO off
 
 CALL ECHO ----------------
-CALL ECHO 1 - [ node - localhost ]
-CALL ECHO 0 - [ git - remove - last commit ]
+CALL ECHO 1 - [ wrangler - dev ]
+CALL ECHO 9 - [ wrangler - deploy ]
+CALL ECHO 0 - [ git - remove last commit ]
 CALL ECHO ----------------
 
 SET /P input="ENTER: "
 
 IF %input% == 1 (
-    CALL node localhost.js
+    CALL npx wrangler pages dev . --port=80
+)
+
+IF %input% == 9 (
+    CALL npx wrangler pages deploy .
 )
 
 IF %input% == 0 (
