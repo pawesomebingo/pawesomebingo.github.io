44ac59c746564cb40c277072dc5abd3faefe9cad
tnguyen-win <129979622+tnguyenwin@users.noreply.github.com>
2026-03-29 19:03:16 -0700

    Fix Typo + Setup Google Verification

--- custom_code/logic.js
+++ custom_code/logic.js
@@ -1,10 +1,44 @@
 // <script src="/custom_code/logic.js"></script>
 
-function removeBadge() {
-    document
-        .querySelector('.w-webflow-badge')
-        ?.style.setProperty('display', 'none', 'important');
-}
+// console.log({
+//   text: [...document.body.children]
+//     .map((e) =>
+//       e.textContent ? e.textContent.trim().replace(/ {3,}/g, "") : "",
+//     )
+//     .map((t) => (t ? t.split(/\n{2,}/) : []))
+//     .map((arr) => arr.map((s) => s.trim()))
+//     .map((arr) => arr.map((s) => s.split(/\n/)))
+//     .map((arr) =>
+//       arr.map((sub) => sub.map((x) => x.trim()).filter((x) => x.length > 0)),
+//     )
+//     .reduce((all, arr) => all.concat(...arr), [])
+//     .filter((t) => t.length > 0),
 
-setTimeout(() => removeBadge(), 10);
-setTimeout(() => removeBadge(), 1000);
+//   images: [...document.body.querySelectorAll("*")]
+//     .filter((e) => e.tagName === "IMG")
+//     .map((e) => {
+//       try {
+//         const u = new URL(e.src);
+//         return u.hostname === location.hostname
+//           ? u.pathname + u.search + u.hash
+//           : u.href;
+//       } catch {
+//         return e.src;
+//       }
+//     })
+//     .filter((src) => src && src.length > 0),
+
+//   links: [...document.querySelectorAll("*")]
+//     .filter((e) => e.href)
+//     .map((e) => {
+//       try {
+//         const u = new URL(e.href);
+//         return u.hostname === location.hostname
+//           ? u.pathname + u.search + u.hash
+//           : u.href;
+//       } catch {
+//         return e.href;
+//       }
+//     })
+//     .filter((href) => href && href.length > 0),
+// });
