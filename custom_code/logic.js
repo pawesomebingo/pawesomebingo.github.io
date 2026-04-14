bc957e829bf0f9fde074eb2f6e274f7972256655
tnguyen-win <129979622+tnguyenwin@users.noreply.github.com>
2026-04-14 10:18:09 -0700

    New Content

--- custom_code/logic.js
+++ custom_code/logic.js
@@ -1,44 +1,15 @@
 // <script src="/custom_code/logic.js"></script>
 
-// console.log({
-//     text: [...document.body.children]
-//         .map(e =>
-//             e.textContent ? e.textContent.trim().replace(/ {3,}/g, '') : ''
-//         )
-//         .map(t => (t ? t.split(/\n{2,}/) : []))
-//         .map(arr => arr.map(s => s.trim()))
-//         .map(arr => arr.map(s => s.split(/\n/)))
-//         .map(arr =>
-//             arr.map(sub => sub.map(x => x.trim()).filter(x => x.length > 0))
-//         )
-//         .reduce((all, arr) => all.concat(...arr), [])
-//         .filter(t => t.length > 0),
+function setNewestDate() {
+    const e = document.querySelector('.heading_event');
+    const d = new Date('2026-05-08');
 
-//     images: [...document.body.querySelectorAll('*')]
-//         .filter(e => e.tagName === 'IMG')
-//         .map(e => {
-//             try {
-//                 const u = new URL(e.src);
-//                 return u.hostname === location.hostname
-//                     ? u.pathname + u.search + u.hash
-//                     : u.href;
-//             } catch {
-//                 return e.src;
-//             }
-//         })
-//         .filter(src => src && src.length > 0),
+    e.textContent = e
+        ? `LIVE IN ${-Math.floor(
+              (Date.now() - new Date('2026-05-08')) / (1000 * 60 * 60 * 24)
+          )} DAYS`
+        : e.textContent;
+}
 
-//     links: [...document.querySelectorAll('*')]
-//         .filter(e => e.href)
-//         .map(e => {
-//             try {
-//                 const u = new URL(e.href);
-//                 return u.hostname === location.hostname
-//                     ? u.pathname + u.search + u.hash
-//                     : u.href;
-//             } catch {
-//                 return e.href;
-//             }
-//         })
-//         .filter(href => href && href.length > 0)
-// });
+setTimeout(() => setNewestDate(), 10);
+setTimeout(() => setNewestDate(), 1000);
