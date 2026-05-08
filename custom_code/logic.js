239449240572ab46a0c151988e2b1bec0a9d35e8
tnguyen-win <129979622+tnguyenwin@users.noreply.github.com>
2026-05-08 14:53:28 -0700

    Improve Date Counter

--- custom_code/logic.js
+++ custom_code/logic.js
@@ -2,13 +2,16 @@
 
 function setNewestDate() {
     const e = document.querySelector('.heading_event');
+    const eventDate = new Date(2026, 4, 8);
+    const today = new Date();
 
-    e.textContent = e
-        ? `LIVE IN ${-Math.floor(
-              (Date.now() - new Date('2026-05-08')) / (1000 * 60 * 60 * 24)
-          )} DAYS`
-        : e.textContent;
+    e.textContent =
+        today < eventDate
+            ? `LIVE IN ${Math.floor((eventDate - today) / (1000 * 60 * 60 * 24))} DAYS`
+            : today.toDateString() === eventDate.toDateString()
+              ? 'LIVE NOW!'
+              : 'EVENT HAS PASSED';
 }
 
-setTimeout(() => setNewestDate(), 10);
-setTimeout(() => setNewestDate(), 1000);
+setTimeout(setNewestDate, 10);
+setTimeout(setNewestDate, 1000);
